598b58f40204e1b2afc03abb40efa78d7b74fab5
linneah863 <lina.ki.27195@gmail.com>
2021-10-12 21:06:30 +0300

    states.Counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Clock from "./components/Clock";
+import MyButton from "./components/MyButton";
+import Counter from "./components/Counter";
+const buttonArray=["pudge","mom"]
+const App =() =>{
+return(
+<div classname="App">
+ 
+<Counter></Counter>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+</div>
+);
 }
 
 export default App;
